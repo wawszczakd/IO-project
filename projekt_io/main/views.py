9c6afca65d68a31060cd1097b1091c9124c4b8a9
Grzegorz Gwóźdź <gg438481@students.mimuw.edu.pl>
2023-05-16 10:02:24 +0200

    add refreshing image to the site

--- projekt_io/main/views.py
+++ projekt_io/main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import time
 
 def index(request):
-    return render(request=request, template_name='index.html')
+    return render(request=request, template_name='main/index.html')
+
+def play(request):
+    return render(request, 'main/play.html', {})
