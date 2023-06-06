98f57244a6c4a5b64fb3a4bea692dab236b21ee9
Grzegorz Gwóźdź <gg438481@students.mimuw.edu.pl>
2023-06-06 21:17:41 +0200

    little model changes

--- projekt_io/game/models.py
+++ projekt_io/game/models.py
@@ -4,6 +4,7 @@ import chess
 
 # Create your models here.
 
+
 class Game(models.Model):
     STATUS_CHOICES = (
         ('O', 'Ongoing'),
@@ -12,28 +13,39 @@ class Game(models.Model):
 
     # 4 players
     players = models.ManyToManyField(User, related_name='games')
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='O')
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, default='O')
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(blank=True, null=True)
-    current_move = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='current_games')
+    current_move = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='current_games')
 
     board = chess.Board()
 
     def get_board(self):
-            return self.board
-    
+        return self.board
+
     def get_legal_moves(self):
-            return self.board.legal_moves    
+        return self.board.legal_moves
 
 
 # specific class extending basic Game
 class GameHandAndBrain(Game):
-    team1_hand = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='team1_games_head')
-    team1_brain = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='team1_games_brain')
-    team2_hand = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='team2_games_head')
-    team2_brain = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='team2_games_brain')
+    team1_hand = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='team1_games_head')
+    team1_brain = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='team1_games_brain')
+    team2_hand = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='team2_games_head')
+    team2_brain = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='team2_games_brain')
 
     current_piece = models.CharField(max_length=10, blank=True, null=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.current_move:
+            self.current_move = self.team1_brain
 
     def get_next_move(self):
         if self.current_move == self.team1_brain:
@@ -54,3 +66,5 @@ class GameHandAndBrain(Game):
     def make_move_hand(self, move):
         self.board.push_san(move)
         self.save()
+
+    
--- projekt_io/main/views.py
+++ projekt_io/main/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from game.models import *
 import time
+
 
 def index(request):
     return render(request=request, template_name='main/index.html')
 
 def play(request):
+    game = GameHandAndBrain()
     return render(request, 'main/play.html', {})
