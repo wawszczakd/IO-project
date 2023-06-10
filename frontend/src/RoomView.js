4ed5bfea9540a6b6415a33a97109994e09d58140
Jakub Dziura <jdziura10@gmail.com>
2023-06-10 14:36:06 +0200

    PI-25 Add team/role support from user side

--- frontend/src/RoomView.js
+++ frontend/src/RoomView.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import GameRoom from "./GameRoom";
 
 function RoomView({ roomCode, nickname }) {
-    const [connectedUsers, setConnectedUsers] = useState([]);
+    const [connectedUsers, setConnectedUsers] = useState({});
     const [userId, setUserId] = useState(null);
     const [ownerId, setOwnerId] = useState(null);
     const [isLobbyVisible, setLobbyVisible] = useState(true);
@@ -22,6 +22,30 @@ function RoomView({ roomCode, nickname }) {
                 socket.send(JSON.stringify(message));
             }
         };
+
+        const handleChangeTeam = (newTeam) => {
+            console.log("changing to team " + newTeam);
+            const message = {
+                type: 'team_changed',
+                user_id: userId,
+                new_team: newTeam,
+            };
+            socket.send(JSON.stringify(message));
+        };
+
+        const handleChangeRole = (newRole) => {
+            const message = {
+                type: 'role_changed',
+                user_id: userId,
+                new_role: newRole,
+            };
+            socket.send(JSON.stringify(message));
+        }
+
+        const handleChangeTeam1 = () => handleChangeTeam(1);
+        const handleChangeTeam2 = () => handleChangeTeam(2);
+        const handleSwitchToHand = () => handleChangeRole("hand");
+        const handleSwitchToBrain = () => handleChangeRole("brain");
 
         const handleClose = () => {
             console.log("Disconnected from socket");
@@ -44,7 +68,7 @@ function RoomView({ roomCode, nickname }) {
                     setConnectedUsers(data.connected_users);
                     setOwnerId(data.owner_id);
                     if (userId == null)
-                        setUserId(data.connected_users[data.connected_users.length - 1].id);
+                        setUserId(data.new_id);
 
                     break;
                 default:
@@ -52,16 +76,29 @@ function RoomView({ roomCode, nickname }) {
             }
         }
 
-        window.addEventListener('beforeunload', handleBeforeunload);
-        socket.addEventListener('open',       handleOpen);
-        socket.addEventListener('close',      handleClose);
-        socket.addEventListener('message',    handleMessage);
+        const join_team_1_btn = document.getElementById("join-team-1-btn");
+        const join_team_2_btn = document.getElementById("join-team-2-btn");
+        const switch_to_hand_btn = document.getElementById("switch-to-hand-btn");
+        const switch_to_brain_btn = document.getElementById("switch-to-brain-btn");
+
+        join_team_1_btn.addEventListener('click',       handleChangeTeam1);
+        join_team_2_btn.addEventListener('click',       handleChangeTeam2);
+        switch_to_hand_btn.addEventListener('click',    handleSwitchToHand);
+        switch_to_brain_btn.addEventListener('click',   handleSwitchToBrain);
+        window.addEventListener('beforeunload',         handleBeforeunload);
+        socket.addEventListener('open',                 handleOpen);
+        socket.addEventListener('close',                handleClose);
+        socket.addEventListener('message',              handleMessage);
 
         return () => {
-            window.removeEventListener('beforeunload', handleBeforeunload);
-            socket.removeEventListener('open',       handleOpen);
-            socket.removeEventListener('close',      handleClose);
-            socket.removeEventListener('message',    handleMessage);
+            join_team_1_btn.removeEventListener('click',        handleChangeTeam1);
+            join_team_2_btn.removeEventListener('click',        handleChangeTeam2);
+            switch_to_hand_btn.removeEventListener('click',     handleSwitchToHand);
+            switch_to_brain_btn.removeEventListener('click',    handleSwitchToBrain);
+            window.removeEventListener('beforeunload',          handleBeforeunload);
+            socket.removeEventListener('open',                  handleOpen);
+            socket.removeEventListener('close',                 handleClose);
+            socket.removeEventListener('message',               handleMessage);
             socket.close();
         }
     }, [nickname, roomCode, userId]);
@@ -77,11 +114,24 @@ function RoomView({ roomCode, nickname }) {
                 {isLobbyVisible && (
                     <div id="join-room-section" className="text-center">
                         <h2 className="text-center">Room: { roomCode }</h2>
-                        <ul>
-                            {connectedUsers.map((user, index) => (
-                                <li key={index}>{user.nickname}</li>
-                            ))}
-                        </ul>
+                        <div className="row">
+                            <div className="col-sm-6">Team 1</div>
+                            <ul>
+                                { Object.keys(connectedUsers).map((key, index) => {
+                                    return connectedUsers[key].team === 1
+                                    ?   <li key={index}>{connectedUsers[key].nickname + " (" + connectedUsers[key].role + ")"}</li>
+                                    :   null
+                                })}
+                            </ul>
+                            <div className="col-sm-6">Team 2</div>
+                            <ul>
+                                { Object.keys(connectedUsers).map((key, index) => {
+                                    return connectedUsers[key].team === 2
+                                    ?   <li key={index}>{connectedUsers[key].nickname + " (" + connectedUsers[key].role + ")"}</li>
+                                    :   null
+                                })}
+                            </ul>
+                        </div>
                         <div className="text-center">
                             <button
                                 type="button"
@@ -94,12 +144,42 @@ function RoomView({ roomCode, nickname }) {
                             <button
                                 type="button"
                                 className="btn btn-danger mx-3"
-                                id="create-room-btn"
+                                id="leave-room-btn"
                                 onClick={() => window.location.reload(false)}
                             >
                                 Leave Room
                             </button>
                         </div>
+
+                        <button
+                            type="button"
+                            className="btn btn-primary mx-3"
+                            id="join-team-1-btn"
+                        >
+                            Join team 1
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-primary mx-3"
+                            id="join-team-2-btn"
+                        >
+                            Join team 2
+                        </button>
+
+                        <button
+                            type="button"
+                            className="btn btn-primary mx-3"
+                            id="switch-to-hand-btn"
+                        >
+                            Switch to hand
+                        </button>
+                        <button
+                            type="button"
+                            className="btn btn-primary mx-3"
+                            id="switch-to-brain-btn"
+                        >
+                            Switch to brain
+                        </button>
                     </div>
                 )}
 
